63e218f7fe85890c78ba09ffc2b098491c3c12c3
simsimi2143 <51520114+simsimi2143@users.noreply.github.com>
2020-10-23 23:18:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
+# la función markdown se utiliza para ocupar funcionalidades de html y css en streamlit
+st.markdown('<style> body {background-color: #33FF8B ;} </style>', unsafe_allow_html = True)
 
+# colores para elejir de fondo de la pagina
+#33FF8B
+#FFCA33
+
+# agragamos una barra latarel que se puede ocultar para alternar entre dos paginas dentro de streamlit
 st.sidebar.title("MENU")
-paginaseleccionada = st.selectbox('seleccione una pagina', ['pagina 1', 'pagina 2'])
+paginaseleccionada = st.sidebar.selectbox('seleccione una pagina', ['pagina 1', 'pagina 2'])
 
+
+# indicamos mediante condicionales lo que ocurrira cuando se selecciones las paginas y los cambios que se generaran al hacerlo
 if paginaseleccionada == 'pagina 1':
     st.title('MONITOREO NACIONAL')
-    st.selectbox('selecione una opción', ['opción 1', 'opción 2'])
+    st.selectbox('selecione una opción', ['I región', 'II región', 'III región',
+         'IV región', 'V región', 'VI región', 'VII región', 'VIII región',
+         'IX región','X región', 'XI región', 'XII región', 'XIII región',
+         'XIV región','XV región'])
+    
 elif paginaseleccionada == 'pagina 2':
-    st.title('usted esta en la pagina 2')
+    st.title('MONITOREO COMUNAL')
