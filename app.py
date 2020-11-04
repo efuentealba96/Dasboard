5a302780a1c185f0b48fce5497bfe160c535fb92
simsimi2143 <51520114+simsimi2143@users.noreply.github.com>
2020-11-03 22:29:04 -0300

    app 2.0
    la nueva actualización del codigo del dashboard cuenta con la implementacion de un mapa el cual sera utilizado para mostar los datos de los csv de la bd en mongo

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # importamos librerias necesarias 
 import streamlit as st
 from PIL import Image
+import pandas as pd
+import numpy as np 
+import pydeck as pdk
 
 # abrimos la imagen
 image = Image.open('head.jpg')
+
 
 # la función markdown se utiliza para ocupar funcionalidades de html y css en streamlit
 st.markdown('<style> body {background-color: #33FF8B ;} </style>', unsafe_allow_html = True)
@@ -20,13 +24,58 @@ paginaseleccionada = st.sidebar.selectbox('seleccione una pagina', ['pagina 1', 
 # indicamos mediante condicionales lo que ocurrira cuando se selecciones las paginas y los cambios que se generaran al hacerlo
 if paginaseleccionada == 'pagina 1':
     st.header('Bienvenidos a nuestro sitio')
+# se inserta una imagne antes del titulo con los pixeles origanel que posee esta
     st.image(image, use_column_width=True)
     st.title('MONITOREO NACIONAL')
+# agregamos un multi-selecionador con el fin de que al momento de hacer consultas del servidor de mongo se puedan seleccionar varias
+# opciones al mismo tiempo
     st.multiselect('selecione una opción', ['I región', 'II región', 'III región',
          'IV región', 'V región', 'VI región', 'VII región', 'VIII región',
          'IX región','X región', 'XI región', 'XII región', 'XIII región',
          'XIV región','XV región'])
+    st.button('mostrar monitoreo')
+# creamos un dataframe para dibujar un mapa
+    df = pd.DataFrame(
+# especificamos es que tipo de forma vamos a trabajar con las direcciones 
+        columns=['lat', 'lon'])
+# usando pydeck dibujamos un mapa segun su estilo y version de esta libreria
+    st.pydeck_chart(pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+# inicializamos la la zona del mapa que queremos ver 
+# todo esto en base a latitud y longitud
+        initial_view_state=pdk.ViewState(
+            latitude=-30.0000000,
+            longitude=-71.0000000,
+            zoom=5,
+# indicamos la altura desde la que visualizara el usuario
+            pitch=80,
+            
+        ),
+# por debajo del mapa se trabaja con una matriz por ende mediante layer
+# especificamos la seccion de la matriz que se visualizara en el mapa
+        layers=[
+            pdk.Layer(
+# formato de datos obtenidos de la matriz
+                'HexagonLayer',
+                data=df,get_position='[lon, lat]',
+                radius=200,elevation_scale=8,elevation_range=[0, 10000],
+                pickable=True,extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=df,
+                get_position='[lon, lat]',
+                get_color='[180, 0, 200, 140]',
+                get_radius=200,
+            ),
+        ],
+    ))
+
+   
+
     
 elif paginaseleccionada == 'pagina 2':
+# le asignamos un nuevo color a esta siguiente pestaña para añadirle mas estilo a esta
     st.markdown('<style> body {background-color: #FFCA33 ;} </style>', unsafe_allow_html = True)
     st.title('MONITOREO COMUNAL')
+    
