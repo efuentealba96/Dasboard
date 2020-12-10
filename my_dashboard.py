c91a0c946b15ee4c4fad4980ba26665ffa3ad377
simsimi2143 <51520114+simsimi2143@users.noreply.github.com>
2020-12-09 23:16:56 -0300

    Update my_dashboard.py

--- my_dashboard.py
+++ my_dashboard.py
@@ -1,3 +1,4 @@
+# impportamos librerias necesarias para el funcionamiento
 from pymongo import MongoClient
 import pandas as pd 
 import streamlit as st
@@ -6,21 +7,27 @@ import datetime
 import plotly.express as px 
 import plotly.graph_objects as gr
 
+# creamos una variable que almacenara la conexión a mongodb
 cliente = MongoClient("mongodb+srv://Eliacer:elia968@cluster0.r0w6h.mongodb.net/Proyecto?retryWrites=true&w=majority")
-
+# indicamos elnombre de la base de datos donde estan almacenados 
+# los datos con los que se trabajaran 
 db = cliente['Proyecto']
 
-
+# creamos nuestra primera función con la cual mostraremos la grafica de las defunciones
 def get_data():
+# indicamos la tabla de la cual obtendrmos la información necesaria 
+# para poder graficar
     colecion = db['Defunciones']
     df = pd.DataFrame(list(colecion.find()))
+# indicamos los campos a tener en consideración para graficar
     df["Año"] = [df['Fecha'][i].split("-")[0] for i in range(df.shape[0])]
     df["Mes"] = [df['Fecha'][i].split("-")[1] for i in range(df.shape[0])]
-    df["Dia"] = [df['Fecha'][i].split("-")[2] for i in range(df.shape[0])] 
+    df["Dia"] = [df['Fecha'][i].split("-")[2] for i in range(df.shape[0])]    
     df = df[[int(df['Año'][i])>=2020 for i in range(df.shape[0])]].reset_index(drop=True)
     n_semana = [datetime.datetime.strptime(df["Fecha"][i], '%Y-%m-%d').date().isocalendar()[1] for i in range(df.shape[0])]
     df['Semana'] = n_semana   
     data = df.groupby(['Año','Semana','Region','Comuna'], as_index=False).sum()
+# indicamos el identificador de cada región y comuna    
     del data['Codigo region']
     del data['Codigo comuna']
     return data
@@ -28,7 +35,9 @@ def get_data():
 def grafica_region(dfs,region):
     dfs = dfs[dfs['Region'] == region]
     fig = gr.Figure()
+# indicamos el color de la grafica    
     color = ["red"]
+# indicamos el metodo por el cual se agruparan los datos
     grupo =dfs.groupby("Año")
     i = 0
     for year,group in grupo:
@@ -49,7 +58,7 @@ def grafica_region(dfs,region):
         height = 500
     )
     return fig
-
+# comenzamos con el llamado de nuestras funciones
 df = get_data()
 st.dataframe(df)
 st.header('Gráfico por regiones')
