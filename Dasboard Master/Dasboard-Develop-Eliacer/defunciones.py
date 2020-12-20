daf680bc4e5f3b0c0e6012d36a919434569c14cb
simsimi2143 <51520114+simsimi2143@users.noreply.github.com>
2020-12-20 18:04:34 -0300

    Dashboard Final
    cambios esteticos asi como la documentación final del código

--- Dasboard Master/Dasboard-Develop-Eliacer/defunciones.py
+++ Dasboard Master/Dasboard-Develop-Eliacer/defunciones.py
@@ -8,7 +8,10 @@ import plotly.graph_objects as gr
 cliente = MongoClient("mongodb+srv://Eliacer:elia968@cluster0.r0w6h.mongodb.net/Proyecto?retryWrites=true&w=majority")
 db = cliente['Proyecto']
 
+# creamos nuestra primera función con la cual mostraremos la grafica de las defunciones
 def get_defunciones():
+# indicamos la tabla de la cual obtendrmos la información necesaria 
+# para poder graficar
     colecion = db['Defunciones']
     df = pd.DataFrame(list(colecion.find()))
     df["Año"] = [df['Fecha'][i].split("-")[0] for i in range(df.shape[0])]
@@ -21,8 +24,11 @@ def get_defunciones():
     return data
 
 @st.cache
+# función para graficar las defunciones
 def grafica_defunciones(df,regiones):
+# se crea una variable en la cual se almacena una figura
     fig = gr.Figure()
+# se indica dentro del for los elementos a tomar para graficar en base a la tabla de region
     for i,region in enumerate(regiones):
         aux  = df[df['Region']==region]
         fig.add_trace(gr.Bar(x=aux['Mes'],y=aux['Defunciones'],name=region,marker_color=px.colors.qualitative.G10[i]))
@@ -36,6 +42,8 @@ def grafica_defunciones(df,regiones):
     return fig
 
 def main():
+# se definen los estilos que tendra la pagina en el fondo asi como en la barra lateral
+# haciendo uso de propiedades de html en streamlit
     st.title("Defunciones durante el año 2020")
     st.markdown('<style> body {background-color: #449A04;}', unsafe_allow_html = True)
     st.markdown("""
@@ -45,6 +53,7 @@ def main():
         }
     </style>""",unsafe_allow_html=True,)
     df = get_defunciones()
+# se indica que dada la condición se ejecute la acción de mostrar los graficos
     if st.checkbox("Mostrar datos graficados"):
         st.dataframe(df)
     st.header('Gráfico por regiones')
